c23e920f09c0c2dda7f71f22ab5b480179c39574
AdrianCraft07 <proadrian684@gmail.com>
2022-09-08 20:34:07 -0500

    use TypeScript

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,20 @@
-import fs from 'fs'
-export function removeDetect(path: fs.PathLike): void;
-export function removeDir(path: fs.PathLike): void;
-export function remove(path: fs.PathLike): void;
-export function isDirectory(path: fs.PathLike): Boolean;
-export function isFile(path: fs.PathLike): Boolean;
-export function directory(path: fs.PathLike): String;
-export function file(path: fs.PathLike, data: String | Buffer): String;
+/// <reference types="node" />
+declare function readFile(path: string, type?: string): Buffer;
+declare function removeDir(path: string): void;
+declare function remove(path: string): void;
+declare function removeDetect(path: string): void;
+declare function isDirectory(path: string): boolean;
+declare function isFile(path: string): boolean;
+declare function directory(path: string): string;
+declare function file(path: string, data: string | NodeJS.ArrayBufferView): string;
+declare const _default: {
+    removeDetect: typeof removeDetect;
+    isDirectory: typeof isDirectory;
+    directory: typeof directory;
+    removeDir: typeof removeDir;
+    readFile: typeof readFile;
+    remove: typeof remove;
+    isFile: typeof isFile;
+    file: typeof file;
+};
+export = _default;
--- index.js
+++ index.js
@@ -1,72 +1,81 @@
-const fs = require('fs');
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const fs_1 = __importDefault(require("fs"));
 function readFile(path, type = '') {
-  const file = fs.readFileSync;
-  let res = Buffer('');
-  if (isDirectory(path)) res = file(`${path}/index.${type}`);
-  else if (path.endsWith(type) && isFile(path)) res = file(path);
-  else if (isFile(`${path}.${type}`)) res = file(`${path}.${type}`);
-  return res;
+    const file = fs_1.default.readFileSync;
+    let res = new Buffer('');
+    if (isDirectory(path))
+        res = file(`${path}/index.${type}`);
+    else if (path.endsWith(type) && isFile(path))
+        res = file(path);
+    else if (isFile(`${path}.${type}`))
+        res = file(`${path}.${type}`);
+    return res;
 }
-
 function removeDir(path) {
-  const files = fs.readdirSync(path);
-  files.filter(file => isDirectory(`${path}/${file}`)).map(dir => removeDir(`${path}/${dir}`));
-  files.filter(file => isFile(`${path}/${file}`)).map(file => remove(`${path}/${file}`));
-  fs.rmdirSync(path);
+    const files = fs_1.default.readdirSync(path);
+    files
+        .filter(file => isDirectory(`${path}/${file}`))
+        .map(dir => removeDir(`${path}/${dir}`));
+    files
+        .filter(file => isFile(`${path}/${file}`))
+        .map(file => remove(`${path}/${file}`));
+    fs_1.default.rmdirSync(path);
 }
 function remove(path) {
-  fs.rmSync(path);
+    fs_1.default.rmSync(path);
 }
 function removeDetect(path) {
-  if (isFile(path) || isFolder(path))
-    try {
-      removeDir(path);
-    } catch (e) {
-      remove(path);
-    }
+    if (isFile(path) || isDirectory(path))
+        try {
+            removeDir(path);
+        }
+        catch (e) {
+            remove(path);
+        }
 }
 function isDirectory(path) {
-  try {
-    fs.readdirSync(path);
-    return true;
-  } catch (e) {
-    return false;
-  }
+    try {
+        fs_1.default.readdirSync(path);
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
 }
 function isFile(path) {
-  try {
-    fs.readFileSync(path);
-    return true;
-  } catch (e) {
-    return false;
-  }
+    try {
+        fs_1.default.readFileSync(path);
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
 }
 function directory(path) {
-  if (fs.existsSync(path)) return path;
-  let pathSplit = path.split(/[\\/]/);
-  directory(pathSplit.filter((_, index) => index < pathSplit.length - 1).join('/'));
-  fs.mkdirSync(path);
-  return path;
+    if (!path)
+        return path;
+    if (fs_1.default.existsSync(path))
+        return path;
+    let pathSplit = path.split(/[\\/]/);
+    directory(pathSplit.filter((_, index) => index < pathSplit.length - 1).join('/'));
+    fs_1.default.mkdirSync(path);
+    return path;
 }
 function file(path, data) {
-  let pathSplit = path.split(/[/\\]/);
-
-  fs.writeFileSync(
-    `${directory(pathSplit.filter((_, index) => index < pathSplit.length - 1).join('/'))}/${
-      pathSplit[pathSplit.length - 1]
-    }`,
-    data
-  );
-  return path;
+    let pathSplit = path.split(/[/\\]/);
+    fs_1.default.writeFileSync(`${directory(pathSplit.filter((_, index) => index < pathSplit.length - 1).join('/'))}/${pathSplit[pathSplit.length - 1]}`, data);
+    return path;
 }
-module['exports'] = {
-  removeDetect,
-  isDirectory,
-  directory,
-  removeDir,
-  readFile,
-  remove,
-  isFile,
-  file,
+module.exports = {
+    removeDetect,
+    isDirectory,
+    directory,
+    removeDir,
+    readFile,
+    remove,
+    isFile,
+    file,
 };
